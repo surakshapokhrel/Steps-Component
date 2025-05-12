e1c163f8ea423e5e044be6bae866491a77ef962f
suraksha pokhrel <suraksha2060@gmail.com>
2025-05-12 16:59:20 +0545

    Adding another piece of state

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -17,20 +18,27 @@ function App() {
     if (step < 3) setStep(step + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message ">
-        Steps{step}:{messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button onClick={handlePrevious}> Previous</button>
-        <button onClick={handleNext}>Next </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message ">
+            Steps{step}:{messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button onClick={handlePrevious}> Previous</button>
+            <button onClick={handleNext}>Next </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
